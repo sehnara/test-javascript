2a8add614984bb8c08701ff1411e2ce1dd6c2911
sehnara <gsh723@naver.com>
2022-10-17 00:59:22 +0900

    asynchronous test

---
+++ src/test/async.test.js
@@ -0,0 +1,33 @@
+const fetchProduct = require("../async");
+
+// 수동으로 done()함수를 발동하면 그 때 test 끝남
+describe("ASYNC", () => {
+  it("async-done", (done) => {
+    fetchProduct().then((res) => {
+      expect(res).toEqual({ item: "Milk", price: 200 });
+      done();
+    });
+  });
+
+  it("async-return", () => {
+    return fetchProduct().then((res) => {
+      expect(res).toEqual({ item: "Milk", price: 200 });
+    });
+  });
+
+  it("async-await", async () => {
+    const product = await fetchProduct();
+    expect(product).toEqual({ item: "Milk", price: 200 });
+  });
+
+  it("async-resolves", () => {
+    return expect(fetchProduct()).resolves.toEqual({
+      item: "Milk",
+      price: 200,
+    });
+  });
+
+  it("async-reject", () => {
+    expect(fetchProduct("error")).rejects.toBe("network error");
+  });
+});
